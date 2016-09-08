6301cb90dc792560e957b209fae846b737ae2b4d
Sam Lanning <sam@semmle.com>
2016-09-08 18:58:15 +0100

    add more java stuff

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 class Main {
 
 	public String foo;
+	public String bar;
+	private String baz;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World" + this.baz);
 	}
 
 }
