552169d7cbe1fcd32c0e1ea9723b28cee87af29c
Sam Lanning <sam@semmle.com>
2016-09-09 11:25:23 +0100

    lgtm: couple more system outs

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World" + new Main().baz);
+		System.out.println("Hello World" + new Main().baz);
+		System.out.println("Hello World" + new Main().baz);
 	}
 
 }
