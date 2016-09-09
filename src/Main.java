ac3ff72a4b4a40f548130cedb9103af73cd68a3c
Sam Lanning <sam@semmle.com>
2016-09-09 11:23:03 +0100

    lgtm: fix java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ class Main {
 	private String baz;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World" + this.baz);
+		System.out.println("Hello World" + new Main().baz);
 	}
 
 }
